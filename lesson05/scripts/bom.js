957c60fc9ba4ecca87149276dc937e80a8b527e7
mafradiv <marco.aprendeinglesya@gmail.com>
2023-10-23 12:05:41 -0500

    chamber

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,11 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+button.addEventListener('click', () => { 
+    if(input.value !=''){
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = ''
+    }
+ })
